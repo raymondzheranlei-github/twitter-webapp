f73a43c0165ebdce918563fa3a8299a1b487e170
Zheran Lei <raymondlzr@Zherans-MacBook-Pro.local>
2021-06-19 14:31:02 -0700

    userProfile model and admin

---
+++ accounts/tests.py
@@ -0,0 +1,12 @@
+from accounts.models import UserProfile
+from testing_utils.testcases import TestCase
+
+
+class UserProfileTests(TestCase):
+
+    def test_profile_property(self):
+        ray = self.create_user('ray')
+        self.assertEqual(UserProfile.objects.count(), 0)
+        p = ray.profile
+        self.assertEqual(isinstance(p, UserProfile), True)
+        self.assertEqual(UserProfile.objects.count(), 1)
