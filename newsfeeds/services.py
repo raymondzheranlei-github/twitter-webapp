3f62f45664a44300945396d8f0dbf59c9cc46045
Zheran Lei <raymondlzr@Zherans-MacBook-Pro.local>
2021-07-11 16:20:28 -0700

    Cache newsfeeds in redis

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,11 +1,13 @@
 from friendships.services import FriendshipService
 from newsfeeds.models import NewsFeed
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 
 
 class NewsFeedService(object):
 
     @classmethod
-    def fanout_to_followers(self, tweet):
+    def fanout_to_followers(cls, tweet):
         newsfeeds = [
             NewsFeed(user=follower, tweet=tweet)
             for follower in FriendshipService.get_followers(tweet.user)
@@ -13,3 +15,19 @@ class NewsFeedService(object):
         newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
         # bulk_create will combine all insert queries into only one query
         NewsFeed.objects.bulk_create(newsfeeds)
+
+        # bulk create will not trigger post_save signal，need to push to cache manually
+        for newsfeed in newsfeeds:
+            cls.push_newsfeed_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeed_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
